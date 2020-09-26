7091c26263aee86b4efd71a7d8241e7601e85fee
Aleksandr Maniunin <aamanunin@gmail.com>
2020-09-26 22:24:44 +0700

    Refactoring home.component

---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Course} from '../model/course';
+import {map, shareReplay} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  constructor(private http: HttpClient) { }
+
+  loadAllCourses(): Observable<Course[]> {
+    return this.http.get<{payload: Course[]}>('/api/courses')
+      .pipe(
+        map(res => res.payload),
+        shareReplay()
+      );
+  }
+}
